951df2e7589204b36816abb4681d6d5ae44d490e
Simon <21604666>
2017-05-04 17:26:00 +0200

    voici la version graphique de l'app andro

--- Android/MyApplication/app/src/main/java/vocs/com/vocs/MainActivity.java
+++ Android/MyApplication/app/src/main/java/vocs/com/vocs/MainActivity.java
@@ -4,12 +4,11 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
-//qzdzd
-    //salut je fais une modification
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        System.out.println("test");
+
     }
 }
