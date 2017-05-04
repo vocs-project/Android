9429e85779115cb0b419fab346b0ac41a3a92459
Simon <21604666>
2017-05-04 16:48:24 +0200

    Test2

--- Android/MyApplication/app/src/main/java/vocs/com/vocs/MainActivity.java
+++ Android/MyApplication/app/src/main/java/vocs/com/vocs/MainActivity.java
@@ -5,6 +5,7 @@ import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 //qzdzd
+    //salut je fais une modification
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
